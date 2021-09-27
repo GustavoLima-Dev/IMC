5ec81b92a087561e9b1b8325fe588c692d98aad4
Gustavo henrique <ghulimalacerda@outlook.com>
2021-09-27 15:21:20 -0300

    upload do exercicio imc com js

--- imc.js
+++ imc.js
@@ -16,19 +16,19 @@ function calc() {
 
   // verifica situação
   if (resultado < 17) {
-    message = 'Você está muito abaixo do peso';
+    message = 'Você está muito abaixo do peso, procure manter seu peso acima de 45Kg';
   } else if (resultado >= 17 && resultado <= 18.49) {
-    message = 'Você está abaixo do peso';
+    message = 'Você está abaixo do peso, procure manter seu peso entre 50Kg e 56Kg';
   } else if (resultado >= 18.5 && resultado <= 24.99) {
-    message = 'Seu peso está normal';
+    message = 'Seu peso está normal, procure manter seu peso entre 56Kg e 67Kg ';
   } else if (resultado >= 25 && resultado <= 29.99) {
-    message = 'Você está acima do peso';
+    message = 'Você está acima do peso, procure manter seu peso entre 65Kg e 75Kg';
   } else if (resultado >= 30 && resultado <= 34.99) {
-    message = 'Você tem obesidade nível I';
+    message = 'Você tem obesidade nível I, procure manter seu peso entre 73Kg e 82Kg';
   } else if (resultado >= 35 && resultado <= 39.99) {
-    message = 'Você tem Obesidade nível II';
+    message = 'Você tem Obesidade nível II, procure manter seu peso entre 80Kg e 86Kg';
   } else if (resultado >= 40) {
-    message = 'Você tem Obesidade nível III';
+    message = 'Você tem Obesidade nível III, procure manter seu peso entre 83Kg e 90Kg';
   }
 
   // mostra resultado
